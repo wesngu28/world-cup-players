3a9c42c5ddb8d8cebae197b8a678500bc821b5bf
Wesley <serpents123@gmail.com>
2022-12-14 12:05:24 -0800

    update csvs, particularly usa vs united states

--- data/nat_vs_birth.py
+++ data/nat_vs_birth.py
@@ -6,23 +6,18 @@ freq_df = pd.read_csv("World Cup Country Statistics.csv")
 wc_countries = wc_df["National_Team"].unique()
 
 def calculate_percentage(country):
-  if country == 'United States':
-    country = 'USA'
+  freq_name = freq_df.loc[freq_df['COUNTRY'] == country]
+  born_in_freq = ''
   if country in wc_countries:
-    if country == 'USA':
-      country = 'United States'
-    freq_name = freq_df.loc[freq_df['COUNTRY'] == country]
-    return freq_name.iloc[0]['FREQUENCY']/26, freq_name.iloc[0]['FREQUENCY']/832
-  else:
-    return
+    born_in_freq = freq_name.iloc[0]['FREQUENCY']/26
+  return born_in_freq, freq_name.iloc[0]['FREQUENCY']/832
 
 def calculate_diff(df):
   countries = df['National_Team'].unique()
   for country in countries:
     diff = 0
-    if country == 'USA':
-      country = 'United States'
     nat_team_players = df.loc[df['National_Team'] == country]
+    print(country)
     for index, row in nat_team_players.iterrows():
       birth = row['COUNTRY']
       if(birth != country):
