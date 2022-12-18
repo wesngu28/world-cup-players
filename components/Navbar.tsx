96637d0299ffb9656f75f92a7b064fad77d05a0a
Wesley <serpents123@gmail.com>
2022-12-18 01:13:15 -0800

    navbar responsive

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,9 +1,9 @@
 export default function Navbar() {
   return(
     <nav>
-      <a className={"p-2"} href="/">Overview</a>
-      <a className={"p-2"} href="/charts">Charts</a>
-      <a className={"p-2"} href="/about">About</a>
+      <a className={"p-2 md:p-4"} href="/">Overview</a>
+      <a className={"p-2 md:p-4"} href="/charts">Charts</a>
+      <a className={"p-2 md:p-4"} href="/about">About</a>
     </nav>
   )
 }
